43e8085a503aa5991339fa243f8baff3468032bd
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-08-20 20:37:52 -0500

    feat: Implement homepage layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='mt-9 flex flex-col gap-9'>
+    <div className="mt-9 flex flex-col gap-9">
+      <section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+        <div className="podcast_grid">
 
-      <section className='flex flex-col gap-5'>
-        <h1 className='text-20 font-bold text-white-1'>
-          Trending Podcasts
-        </h1>
+        {podcastData.map(({ imgURL, id, title, description }) => (
+          <PodcastCard
+          key={id}
+          imgURL={imgURL}
+          title={title}
+          description={description}
+          podcastId={id}
+          />
+        ))}
+        </div>
       </section>
-       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
